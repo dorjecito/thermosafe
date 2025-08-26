a255925621133fc56ec88c0cb73ea6c7e5052865
Esteve Montalvo <esteve.montalvo@gmail.com>
2025-08-26 17:04:09 +0200

    canvi nom manifestper compatibilitat amb Bubblewrap2

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,25 @@
-import './i18n'; // 👈 Això inicialitza el sistema de traducció
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import './i18n'; // Això inicialitza el sistema de traducció
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
+
+// 🔔 Registre del Service Worker
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/firebase-messaging-sw.js')
+      .then(registration => {
+        console.log('✅ Service Worker registrat correctament:', registration);
+      })
+      .catch(error => {
+        console.error('❌ Error en registrar el Service Worker:', error);
+      });
+  });
+}
