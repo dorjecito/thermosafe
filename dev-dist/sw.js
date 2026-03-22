1dcff0143bd0cf5a43baed8dcd1d8a3fa96f66d8
Esteve Montalvo <esteve.montalvo@gmail.com>
2026-03-22 10:03:05 +0100

    Correcions interface i json.22.03.26

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.ba6tjra1f48"
+    "revision": "0.tdrg7uh7c"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
